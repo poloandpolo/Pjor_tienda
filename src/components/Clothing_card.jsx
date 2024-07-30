132a23c0b8c06ad7ecc4083db3c0330b83eebced
Agustin Condado <agustin.condado01@gmail.com>
2024-07-30 13:22:16 -0600

    Cart button added

--- src/components/Clothing_card.jsx
+++ src/components/Clothing_card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './styles/Clothing_card.css'
+import add_to_cart_icon from '/bolsa-de-la-compra.png'
 
 export const Clothing_card = ({imagen, text}) => {
   return (
     <div className='clothing-card-container'>
         <img className='clothing-image' src={imagen} alt="clothing-image"/>
+        <img className='add-to-cart-icon' src={add_to_cart_icon} alt="" />
         <label>{text}</label>
     </div>
   )
